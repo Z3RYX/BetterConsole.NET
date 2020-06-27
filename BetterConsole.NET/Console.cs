23b5c2958a2a86c415200f907bfa0db0025d82f3
Z3RYX <zeryxofficial@gmail.com>
2020-06-27 20:40:26 +0200

    Added Write method and color setting

--- BetterConsole.NET/Console.cs
+++ BetterConsole.NET/Console.cs
@@ -1,9 +1,25 @@
-﻿using System;
-
-namespace BetterConsole
+﻿namespace BetterConsole
 {
-    public class Console
+    public static class Console
     {
-        // TBA
+        static private System.ConsoleColor _ForegroundColor { get { return System.Console.ForegroundColor; } set { System.Console.ForegroundColor = value; } }
+        static private System.ConsoleColor _BackgroundColor { get { return System.Console.BackgroundColor; } set { System.Console.BackgroundColor = value; } }
+
+        public static void Write(object Value, System.ConsoleColor? Color = null, System.ConsoleColor? BackgroundColor = null)
+        {
+            var defaultColors = (_ForegroundColor, _BackgroundColor);
+
+            SetColor(Color, BackgroundColor);
+
+            System.Console.Write(Value.ToString());
+
+            SetColor(defaultColors._ForegroundColor, defaultColors._BackgroundColor);
+        }
+
+        private static void SetColor(System.ConsoleColor? ForegroundColor, System.ConsoleColor? BackgroundColor)
+        {
+            _BackgroundColor = BackgroundColor ?? _BackgroundColor;
+            _ForegroundColor = ForegroundColor ?? _ForegroundColor;
+        }
     }
 }
