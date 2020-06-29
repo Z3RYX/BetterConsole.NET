4c3cf944d7fd658da5e382035bc130e95da5c1dc
Z3RYX <zeryxofficial@gmail.com>
2020-06-29 17:19:53 +0200

    Added ReadLine + removed BG color parameter from Write and WriteLine

--- BetterConsole.NET/Console.cs
+++ BetterConsole.NET/Console.cs
@@ -5,7 +5,7 @@
         static private System.ConsoleColor _ForegroundColor { get { return System.Console.ForegroundColor; } set { System.Console.ForegroundColor = value; } }
         static private System.ConsoleColor _BackgroundColor { get { return System.Console.BackgroundColor; } set { System.Console.BackgroundColor = value; } }
 
-        public static void Write(object Value, System.ConsoleColor? Color = null, System.ConsoleColor? BackgroundColor = null)
+        public static void Write(object Value, System.ConsoleColor? Color = null)
         {
             if (Value.GetType() == typeof(ColoredText))
             {
@@ -16,27 +16,36 @@
                 foreach (var snippet in snippets)
                 {
                     if (System.Console.CursorLeft != 0 && snippets.Spaced) Write(' ');
-                    Write(snippet.Value, snippet.Color, snippet.BackgroundColor);
+                    Write(snippet.Value, snippet.Color);
                     return;
                 }
             }
 
             var defaultColors = (_ForegroundColor, _BackgroundColor);
 
-            SetColor(Color, BackgroundColor);
+            SetColor(Color);
 
             System.Console.Write(Value.ToString());
 
             SetColor(defaultColors._ForegroundColor, defaultColors._BackgroundColor);
         }
 
-        public static void WriteLine(object Value, System.ConsoleColor? Color, System.ConsoleColor? BackgroundColor)
+        public static void WriteLine(object Value, System.ConsoleColor? Color = null)
         {
-            Write(Value, Color, BackgroundColor);
+            Write(Value, Color);
             Write('\n');
         }
 
-        private static void SetColor(System.ConsoleColor? ForegroundColor, System.ConsoleColor? BackgroundColor)
+        public static string ReadLine() => System.Console.ReadLine();
+
+        public static T ReadLine<T>()
+        {
+            string input = System.Console.ReadLine();
+
+            return (T)System.Convert.ChangeType(input, typeof(T));
+        }
+
+        public static void SetColor(System.ConsoleColor? ForegroundColor = null, System.ConsoleColor? BackgroundColor = null)
         {
             _BackgroundColor = BackgroundColor ?? _BackgroundColor;
             _ForegroundColor = ForegroundColor ?? _ForegroundColor;
